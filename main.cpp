5edd46c701188d866f8e45fb3ea481d1ddbe956c
Sprite_Spirit <a.backer@yandex.com>
2023-01-17 15:33:32 +0300

    done

---
+++ main.cpp
@@ -0,0 +1,42 @@
+/* директива препроцессора #include - с помощью этой директивы мы подключаем заголовочный файл iostream
+ * кот. позволяет работать с потоками ввода-вывода
+ * using namespace std; - с помощью этой директивы мы устанавливаем стандартное пространство имен sstd
+ * т.к. каждая программа и программная библиотека библиотека используют свое пространство имен
+ * В C++ нет функци вывода-ввода, но есть операторы выводв-ввода: << и >>
+ * cout - Console Out
+ * cin - Console Input
+ * код возврата 0 - соответсвует отсутствию ошибок, любое другое значение означает код ошибки
+ */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    /*string name;
+    cout << "Enter name:";
+    cin >> name;
+    cout << "Hello, " << name << "\n";
+
+    int number;
+    cout << "Type number:";
+    cin >> number;
+    cout << "You're input number is " << number << endl;
+
+    int one = 7, sum = 0, two = 2;
+    sum = one + two;
+    cout << "Sum numbers is " << sum << endl;
+
+    int first, second, summary;
+
+    cout << "Input two integers:" << endl;
+    cin >> first >> second;
+
+    summary = first + second;
+    cout << "Result: " << first << " + " << second << " = " << summary << endl;*/
+
+    // модификатор endl означает конец строки и переход на новую строку
+
+
+    return 0;
+}
+
