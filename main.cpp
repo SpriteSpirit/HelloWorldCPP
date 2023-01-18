7bc093303ce088cbd2da71693f4a6cf6f06a4362
Sprite_Spirit <a.backer@yandex.com>
2023-01-18 15:09:46 +0300

    Total main1

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@
 using namespace std;
 
 int main() {
-    /**/string name;
+    /*string name;
     cout << "Enter name:";
     cin >> name;
     cout << "Hello, " << name << "\n";
@@ -32,13 +32,67 @@ int main() {
     cin >> first >> second;
 
     summary = first + second;
-    cout << "Result: " << first << " + " << second << " = " << summary << endl;
+    cout << "Result: " << first << " + " << second << " = " << summary << endl;*/
 
     // модификатор endl означает конец строки и переход на новую строку
 
+    /*
+     * 7 базовых типов данных:
+     * 1. char - символьный
+     * 2. wchar_t - символьный двухбайтовый
+     * 3. bool - логический
+     * 4. int - целочисленный
+     * 5. float - вещественный
+     * 6. double - вещественный двойной точности
+     * 7. void - пустой
+     *
+     * Модификаторы типа:
+     * (ключевые слова, кот могут указываться перед именем базового типа и модифицировать его)
+     * signed - значение со знаком
+     * unsigned - значение без знака
+     * short - укороченный
+     * long - расширенный
+     * long long - целевой тип, имеет размер не более 64 бит (С++ 11)
+     *
+     * Числовые литералы:
+     * тип float 5.3F
+     * тип long double 5.3L
+     * тип long int 123L
+     * целочисленный литерал без знака 123U
+     */
+    /*
+
+    char parameter1 = 'A'; // символы должны быть заключены в одинарные кавычки
+    char parameter2 = 'B';
+    char result = parameter1 + parameter2;
+    char g = 131;
+
+    cout << "Your character is " << result << " has index " << parameter1 + parameter2 << endl; // Г
+    cout << g << endl;
+
+    bool flag;
+    flag = true_type();
+
+    //flag = 0;
+
+    cout << "Flag is " << boolalpha << flag; // boolalpha - манипулятор для вывода именного значения true/false
 
 
+    cout << "Size of char: " << sizeof(char) << " byte" << endl;
+    cout << "Size of int: " << sizeof(int) << " byte" << endl;
+    cout << "Size of float: " << sizeof(float) << " byte" << endl;
+    cout << "Size of double: " << sizeof(double) << " byte" << endl;
+    cout << "Size of long int: " << sizeof(long int) << " byte" << endl;
 
-    return 0;
+    const float gravity = 9.8;
+    cout << "Gravity is " << gravity;
+
+    // приведение типов
+
+     char ch;
+     cout << "Type symbol:";
+     cin >> ch;
+     cout << "ASCII: " << int(ch) << endl;
+    */
 }
 
---
+++ main2.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    // оператор и операнд
+    /*
+     * Оператор - наименьшая автономная часть языка, кот. выполняет какое-либо действие.
+     * То, с чем работает оператор называется операндами.
+     * Операнд - аргумент оператора
+     *
+     * Операторы бывают унарные и бынарные.
+     * Бинарные - выполняют действия над двумя операндами.
+     * Унарные - выполняют действия над одним операндом (инкремент, декремент.
+     *
+     */
+
+    /*
+    int a = 9, b = 4;
+    cout << a << " / " << b << " = " << a / b << endl;
+
+    float c = 9.0F, d = 4.0F;
+    cout << fixed << setprecision(1) << c << " / " << fixed << setprecision(1) << d << " = " << fixed << setprecision(2) << c / d << endl;
+
+    int e = 9;
+    float f = 4.0F;
+    cout << e << " / "  << fixed << setprecision(1) << f << " = " << fixed << setprecision(2)  << e / f<< endl;
+
+    float g = 9.0F;
+    int h = 4;
+    cout << fixed << setprecision(1) << g << " / " << fixed << h << " = " << fixed << setprecision(2)  << g / h << endl;
+
+
+// Пользователь вводит 2 числа, а программа переставляет их местами
+// Вариант 1. Используем временную переменную такого же типа данных, переменная - "пузырек"
+    int num1 = 5, num2 = 10, temp;
+
+    cout << "Before: " << endl;
+    cout << "num1 = " << num1 << ", num2 = " << num2 << endl;
+
+    temp = num1; // temp = 5
+    num1 = num2; // num1 = 10
+    num2 = temp; // num2 = 5
+
+    cout << "After: " << endl;
+    cout << "num1 = " << num1 << ", num2 = " << num2 << endl;
+// Вариант 2.
+    int num3 = 1, num4 = 2;
+
+    cout << "\nBefore: " << endl;
+    cout <<"num3 = " << num3 << ", num4 = " << num4 << endl;
+
+    num3 = num3 + num4; // 1 + 2; num3 = 3
+    num4 = num3 - num4; // 3 - 2; num4 = 1
+    num3 = num3 - num4; // 3 - 1; num3 = 2
+
+    cout << "After: " << endl;
+    cout << "num3 = " << num3 << ", num4 = " << num4 << endl;
+
+
+    double first, second, result;
+    cout << "Type two numbers: " << endl;
+    cin >> first >> second;
+
+    result = first * second;
+
+    cout << "Result: " << result << endl;
+
+// Использование постфиксной и префиксной форм определяет то, что сначала  будет выполнено:
+// вычисление выражения или изменение значения переменной
+    int x = 2, i = 3;
+    int y = ++x; // сначала увеличивается значение х и только потом у присваивается значение
+    int s = --i; // сначала уменьшается значение у и только потом х присваивается значение
+
+    cout << "X = " << x << ", Y = " << y << endl; // X = 3, Y = 3
+    cout << "S = " << s << ", I = " << i << endl; // S = 2, I = 2
+
+    int z = 5;
+    int q = z++; // сначала q присваивается значение z и только потом увеличивается значение z
+
+    cout << "Q = " << q << ", Z = " << z << endl; //Q = 5, Z = 6
+    cout << "Z = " << z << ", Q = " << q << endl; //Z = 6, Q = 5
+
+    */
+
+} // int main()
